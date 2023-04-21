69806e307a02b8b56c5841ab1dbf1d3cd3b53b02
gizz00 <131011744+gizz00@users.noreply.github.com>
2023-04-21 19:49:48 +0200

    Add files via upload

---
+++ CypressTesting/http_request_replacement/zaciatok.js
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+
+beforeEach( () => {
+
+  cy
+    .server()
+
+  cy
+    .route({
+      method: 'GET'
+      url: '/api/boards', 
+      response: []
+    })
+    .as('boardList')
+
+  cy
+    .route({
+      method: 'POST',
+      url: '/api/boards',
+      response: [],
+      status: 500
+    })
+    .as('createBoard')
+
+  cy
+    .visit('/');
+
+});
+
+it('prazdny zoznam boardov', () => {
+
+})
+
+it.only('chybova hlaska pri vytvoreni boardu', () => {
+
+  cy
+    .get('#new-board')
+    .click()
+
+  cy
+    .get('.board_addBoard')
+    .type('nova zahrada')
+
+  cy
+    .contains('Save')
+    .click()
+
+  cy
+    .get('#errorMessage')
+    .should('be.visible')
+
+})
